0b59c676e59989b3d0b0f573d216f9cfc1d27366
Urmat Jenaliev <sonkei@y.ru>
2021-09-19 01:26:53 +0600

    Avoid hardcode

--- Server/src/app/src/MinimalApplication.scala
+++ Server/src/app/src/MinimalApplication.scala
@@ -5,6 +5,8 @@ import scalatags.Text.all._
 object MinimalApplication extends cask.MainRoutes {
     val bootstrap = "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.css"
 
+    var messages: Seq[(String, String)] = Vector(("alice", "Hello World!"), ("bob", "I am cow, hear me moo"))
+
     @cask.get("/")
     def hello(): doctype = doctype("html")(
         html(
@@ -13,8 +15,14 @@ object MinimalApplication extends cask.MainRoutes {
             ),
             body(
                 div(cls := "container")(
-                    h1("Hello,"),
-                    p("World!")
+                    h1("Scala chat!"),
+                    div(
+                        for ((name, msg) <- messages) yield p(b(name), " ", msg)
+                    ),
+                    div(
+                        input(`type` := "text", placeholder := "User name"),
+                        input(`type` := "text", placeholder := "Write message!"),
+                    )
                 )
             )
         )
